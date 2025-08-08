97b11d790eb3b7bac8632a97bca1caeaf57d1db0
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-08-08 14:25:32 +0000

    fix some details

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-set -e
 
-SYSTEM_UNDER_TEST_URL="${SYSTEM_UNDER_TEST_URL:-http://diva:8080/diva/}"
-APPTOKEN_VERIFIER_URL="${APPTOKEN_VERIFIER_URL:-http://login:8080/login/}"
-IDPLOGIN_URL="${IDPLOGIN_URL:-http://idplogin:8080/idplogin/}"
-GATEKEEPER_SERVER_URL="${GATEKEEPER_SERVER_URL:-http://gatekeeper:8080/gatekeeperserver/}"
+: "${BASE_URL:-http://diva:8080/diva/}"
+: "${LOGIN_URL:-http://login:8080/login/}"
+: "${IDP_LOGIN_URL:-http://idplogin:8080/idplogin/}"
+: "${GATEKEEPER_SERVER_URL:-http://gatekeeper:8080/gatekeeperserver/}"
 
-JAVA_CMD=(
-  java
+exec java
   -classpath /fitnesse/divacorafitnesse.jar
-  -DsystemUnderTestUrl="$SYSTEM_UNDER_TEST_URL"
-  -DappTokenVerifierUrl="$APPTOKEN_VERIFIER_URL"
-  -DidpLoginUrl="$IDPLOGIN_URL"
+  -DsystemUnderTestUrl="$BASE_URL"
+  -DappTokenVerifierUrl="$LOGIN_URL"
+  -DidpLoginUrl="$IDP_LOGIN_URL"
   -DgatekeeperServerUrl="$GATEKEEPER_SERVER_URL"
   fitnesseMain.FitNesseMain
   -p 8090
   -a /fitnesse/password.txt
-)
-
-echo "Running command: ${JAVA_CMD[@]}"
-exec "${JAVA_CMD[@]}"
