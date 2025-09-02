62378fa768da14a6cb67e87a216268c5f618ec94
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-09-02 12:55:09 +0000

    fix contextRoot fitnesse

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
+: "${CONTEXT_ROOT_ARG:=someContextRoot}"
 : "${BASE_URL:-http://diva:8080/diva/}"
 : "${LOGIN_URL:-http://login:8080/login/}"
 : "${IDP_LOGIN_URL:-http://idplogin:8080/idplogin/}"
 : "${GATEKEEPER_SERVER_URL:-http://gatekeeper:8080/gatekeeperserver/}"
+
+sed -i "s|{CONTEXT_ROOT}|${CONTEXT_ROOT_ARG}|g" ./plugins.properties
 
 java \
   -classpath /fitnesse/divacorafitnesse.jar \
